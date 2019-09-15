ac8a8534fd167cd90beb6d5e5c51c1331bf13f39
Ubuntu <ubuntu@ip-172-31-30-137.ec2.internal>
2019-09-15 16:21:59 +0000

    corrections made to hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,13 @@
-"""
-Your module description
-"""
+#!/usr/bin/env python3
+
+# created by: Trinity Armstrong
+# created on: September 2019
+# this is the "Hello, World!" program, but with nice style
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
